71fe7b70ffea52d2f192af2e87e5d634d2d5fbae
Thomas Choi <thomas.choi@neuralmatrixllc.com>
2025-01-08 00:49:32 -0700

    Include the fix of FX rates on shares calculations

--- rebalance/views.py
+++ rebalance/views.py
@@ -7,6 +7,7 @@ import main.dataUtil as DU
 import pandas as pd
 import logging
 import json
+import math
 
 def weights_by_port_name(request):
     port_name = request.GET.get('pid')
@@ -14,6 +15,12 @@ def weights_by_port_name(request):
     print(f'weights_by_port_name({port_name}, {port_value})')
     weights = Weight.objects.using('trading').filter(port_name=port_name)
     return render(request, 'weights_table.html', {'weights': weights})
+
+def calc_shares(row):
+    if row['last'] > 0:
+        return round(row['amount'] / row['last'])
+    else:
+        return 0
 
 def get_weights_details(request):
     id = request.GET.get('id')
@@ -24,16 +31,27 @@ def get_weights_details(request):
         'weights': weight.weights_list  # Use the property to get the parsed weights
     }
     w_df = weight.get_weights_df()
+    logging.debug("=====  Pre-Process of Shares =====")
+    logging.debug(w_df)
+    w_sum = w_df['weight'].sum()
+    if w_sum > 1:
+        logging.error(f"Port_ID:({id}) has total weight: {w_sum} > 1")
     sym_list = ','.join(map("'{0}'".format, w_df.index.to_list()))
-    sql_str = f"SELECT Symbol, last FROM GlobalMarketData.snapshot  where Symbol in ({sym_list});"
+    # sql_str = f"SELECT Symbol, last FROM GlobalMarketData.snapshot  where Symbol in ({sym_list});"
+    slist2 = sym_list.replace("'", "\\'")
+    sql_str = f"call GlobalMarketData.Symbol_FX_rates(\'{slist2}\');"
     logging.debug(sql_str)
     last_df = DU.load_df_SQL(sql_str)
     last_df = last_df.set_index(['Symbol'])
+    logging.debug("===== last_df from DB =====")
     logging.debug(last_df)
-    w_df['amount'] = w_df['weight'] * port_value
+    w_df['amount'] = (w_df['weight'] * port_value).apply(math.floor)
+    # w_df['amount'] = (w_df['weight'] * port_value).round(2)
     if len(last_df)>0:
-        w_df['last'] = last_df['last']
-        w_df['shares'] = (w_df['amount'] / w_df['last']).round(0)
+        w_df['Currency'] = last_df['Currency']
+        w_df['rate'] = last_df['rate']
+        w_df['last'] = round(last_df['last'] / w_df['rate'], 4)
+        w_df['shares'] = w_df.apply(calc_shares, axis=1)
     else:
         w_df['last'] = 0.0
         w_df['shares'] = 0
@@ -41,7 +59,8 @@ def get_weights_details(request):
     logging.debug(w_df)
     df_dict = w_df.to_dict(orient='records')
     data = {
-        'weights': df_dict  # Use the property to get the parsed weights
+        'weights': df_dict,  # Use the property to get the parsed weights
+        'total_w': w_sum
     }
     return JsonResponse(data)
 
